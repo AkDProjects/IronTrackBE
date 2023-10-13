1d45be3130ce9be75ad28749612de93eeac32fd5
canasmh <canasmh@yahoo.com>
2023-10-13 02:41:59 -0400

    Change status code in tokenController

--- src/main/java/com/IronTrack/IronTrackBE/Controllers/TokenController.java
+++ src/main/java/com/IronTrack/IronTrackBE/Controllers/TokenController.java
@@ -3,6 +3,9 @@ package com.IronTrack.IronTrackBE.Controllers;
 import com.IronTrack.IronTrackBE.Models.TokenResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +17,8 @@ import lombok.RequiredArgsConstructor;
 public class TokenController {
     @GetMapping
     public ResponseEntity<TokenResponse> checkToken() {
-
         TokenResponse response = new TokenResponse();
         response.setAuthenticated(true);
-        return ResponseEntity.status(HttpStatus.ACCEPTED.value()).body(response);
+        return ResponseEntity.status(HttpStatus.OK.value()).body(response);
     };
 }
