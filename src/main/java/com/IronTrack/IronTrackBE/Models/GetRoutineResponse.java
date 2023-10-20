51edf741787734dd49d328d5e3dc169c68756a13
canasmh <canasmh@yahoo.com>
2023-10-19 22:14:56 -0400

    Set up routine response

--- src/main/java/com/IronTrack/IronTrackBE/Models/GetRoutineResponse.java
+++ src/main/java/com/IronTrack/IronTrackBE/Models/GetRoutineResponse.java
@@ -1,2 +1,15 @@
-package com.IronTrack.IronTrackBE.Models;public class GetRoutineResponse {
+package com.IronTrack.IronTrackBE.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetRoutineResponse {
+
+    private Routine routine;
 }
