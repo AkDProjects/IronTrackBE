63567bd014da30278d33df869a44f8ef94d2d790
canasmh <canasmh@yahoo.com>
2023-10-20 23:59:56 -0400

    Set up Routine controller
    This controller should contain all the methods needed for Feature 2

--- src/main/java/com/IronTrack/IronTrackBE/Controllers/RoutineController.java
+++ src/main/java/com/IronTrack/IronTrackBE/Controllers/RoutineController.java
@@ -1,2 +1,40 @@
-package com.IronTrack.IronTrackBE.Controllers;public class RoutineController {
+package com.IronTrack.IronTrackBE.Controllers;
+
+import com.IronTrack.IronTrackBE.Repository.Entities.RoutineExercisesEntity;
+import com.IronTrack.IronTrackBE.Services.RoutineService
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/routines/{routine_id}")
+@RequiredArgsConstructor
+public class RoutineController {
+
+    private final RoutineService service;
+
+    @GetMapping("/edit/{routine_exercise_id}")
+    public ResponseEntity<?> getRoutineExercise(
+            @PathVariable("routine_id") Long routineId,
+            @PathVariable("routine_exercise_id") Long routineExerciseId
+    ) {
+        ErrorResponse errorResponse= new ErrorResponse();
+        RoutineExercisesEntity response;
+        try {
+            response = service.getRoutineExercise(routineId, routineExerciseId);
+            return ResponseEntity.ok(response);
+        } catch (NullPointerException e) {
+            errorResponse.setStatusCode(HttpStatus.NOT_FOUND.value());
+            errorResponse.setMessage(e.getMessage());
+        } catch (SecurityException e) {
+            errorResponse.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+            errorResponse.setMessage(e.getMessage());
+        }
+
+        return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
+    }
 }
--- src/main/java/com/IronTrack/IronTrackBE/Models/GetRoutineExerciseResponse.java
+++ src/main/java/com/IronTrack/IronTrackBE/Models/GetRoutineExerciseResponse.java
@@ -1,2 +1,16 @@
-package com.IronTrack.IronTrackBE.Models;public class GetRoutineExerciseResponse {
+package com.IronTrack.IronTrackBE.Models;
+
+import com.IronTrack.IronTrackBE.Repository.Entities.RoutineExercisesEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetRoutineExerciseResponse {
+
+    private RoutineExercisesEntity routineExericse;
 }
