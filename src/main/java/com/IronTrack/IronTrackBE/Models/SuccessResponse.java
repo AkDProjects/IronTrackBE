22df89df27e2abe071b37d0cf50dcd239948a466
canasmh <canasmh@yahoo.com>
2023-10-23 19:14:54 -0400

    Create Success Response Modal
    This model is much like the error response, with a message property and status code property

---
+++ src/main/java/com/IronTrack/IronTrackBE/Models/SuccessResponse.java
@@ -0,0 +1,15 @@
+package com.IronTrack.IronTrackBE.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessResponse {
+    private String message;
+    private int statusCode;
+}
