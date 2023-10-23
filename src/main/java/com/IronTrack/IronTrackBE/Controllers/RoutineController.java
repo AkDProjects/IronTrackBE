86a28c0058d0fc42fdd68709197b1fa9359010af
canasmh <canasmh@yahoo.com>
2023-10-23 13:56:01 -0400

    Update controller and service
    Include all business logic, most of which was copied from PR#5, since they are so similar

--- src/main/java/com/IronTrack/IronTrackBE/Controllers/RoutineController.java
+++ src/main/java/com/IronTrack/IronTrackBE/Controllers/RoutineController.java
@@ -1,2 +1,48 @@
-package com.IronTrack.IronTrackBE.Controllers;public class RoutineController {
+package com.IronTrack.IronTrackBE.Controllers;
+
+import com.IronTrack.IronTrackBE.Models.RoutineExercise;
+import com.IronTrack.IronTrackBE.Services.RoutineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/routines/{routine_id}")
+@RequiredArgsConstructor
+public class RoutineController {
+
+    private final RoutineService service;
+
+    @PutMapping("/edit/{routine_exercise_id}")
+    public ResponseEntity<?> updateRoutineExercise(
+            @PathVariable("routine_id") Long routineId,
+            @PathVariable("routine_exercise_id") Long routineExerciseId,
+            @RequestBody RoutineExercise routineExercise
+    ) {
+        ErrorResponse errorResponse= new ErrorResponse();
+
+        try {
+            service.updateRoutineExercise(routineId, routineExerciseId, routineExercise);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException e) {
+            errorResponse.setStatusCode(HttpStatus.NOT_FOUND.value());
+            errorResponse.setMessage(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            errorResponse.setMessage(e.getMessage());
+        } catch (SecurityException e) {
+            errorResponse.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+            errorResponse.setMessage(e.getMessage());
+        } catch (Exception e) {
+            errorResponse.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            errorResponse.setMessage(e.getMessage());
+        }
+
+        return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
+
+
+    }
 }
--- src/main/java/com/IronTrack/IronTrackBE/Services/RoutineService.java
+++ src/main/java/com/IronTrack/IronTrackBE/Services/RoutineService.java
@@ -1,2 +1,112 @@
-package com.IronTrack.IronTrackBE.Services;public class RoutineService {
+package com.IronTrack.IronTrackBE.Services;
+
+import com.IronTrack.IronTrackBE.Models.Exercise;
+import com.IronTrack.IronTrackBE.Models.RoutineExercise;
+import com.IronTrack.IronTrackBE.Repository.Entities.ExerciseEntity;
+import com.IronTrack.IronTrackBE.Repository.Entities.RoutineEntity;
+import com.IronTrack.IronTrackBE.Repository.Entities.RoutineExercisesEntity;
+import com.IronTrack.IronTrackBE.Repository.Entities.UserEntity;
+import com.IronTrack.IronTrackBE.Repository.ExerciseRepo;
+import com.IronTrack.IronTrackBE.Repository.RoutineExercisesRepo;
+import com.IronTrack.IronTrackBE.Repository.RoutineRepo;
+import com.IronTrack.IronTrackBE.Repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RoutineService {
+
+    private final UserRepo userRepo;
+    private final RoutineRepo routineRepo;
+    private final RoutineExercisesRepo routineExercisesRepo;
+    private final ExerciseRepo exerciseRepo;
+
+    public void updateRoutineExercise(Long routineId, Long routineExerciseId, RoutineExercise routineExercise) throws NullPointerException, SecurityException, Exception {
+        RoutineEntity routine = getRoutineEntity(routineId);
+        RoutineExercisesEntity newRoutineExercise = getRoutineExercisesEntity(routine, routineExerciseId);
+        ExerciseEntity exerciseEntity = getExerciseEntity(routineExercise);
+        try {
+            newRoutineExercise.setExerciseEntity(exerciseEntity);
+            newRoutineExercise.setQuantityUnit(routineExercise.getQuantityUnit());
+            newRoutineExercise.setQuantity(routineExercise.getQuantity());
+            newRoutineExercise.setSets(routineExercise.getSets());
+            newRoutineExercise.setWeight(routineExercise.getWeight());
+            routineExercisesRepo.save(newRoutineExercise);
+        } catch (Exception ex) {
+            throw new Exception("There was a problem updating the routine exercise");
+        }
+    }
+
+    private UserEntity getUserEntity() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.isAuthenticated()) {
+
+            return userRepo.findByEmail(auth.getName())
+                    .orElseThrow(() -> new UsernameNotFoundException("Invalid user email"));
+        }
+
+        throw new SecurityException("User is not authenticated");
+    }
+
+    private ExerciseEntity getExerciseEntity(RoutineExercise routineExercise) {
+        Exercise exercise = routineExercise.getExercise();
+        Optional<ExerciseEntity> exerciseEntity = exerciseRepo.findByName(exercise.getName());
+
+        if (exerciseEntity.isPresent()) {
+            return exerciseEntity.get();
+        } else {
+            ExerciseEntity newExerciseEntity = new ExerciseEntity();
+            newExerciseEntity.setMuscle(exercise.getMuscle());
+            newExerciseEntity.setName(exercise.getName());
+            newExerciseEntity.setType(exercise.getType());
+            newExerciseEntity.setEquipment(exercise.getEquipment());
+            newExerciseEntity.setDifficulty(exercise.getDifficulty());
+            newExerciseEntity.setInstructions(exercise.getInstructions());
+            return exerciseRepo.save(newExerciseEntity);
+        }
+
+    }
+
+    private RoutineEntity getRoutineEntity(Long routineId) throws NoSuchElementException, SecurityException {
+
+        UserEntity user = getUserEntity();
+        UserEntity routineUser;
+        RoutineEntity routine = routineRepo.findById(routineId);
+
+        if (routine == null) {
+            throw new NoSuchElementException("Routine was not found");
+        }
+
+        routineUser = routine.getUserEntity();
+
+        if (routineUser != user) {
+            throw new SecurityException("User does not own routine");
+        }
+
+        return routine;
+    }
+
+    private RoutineExercisesEntity getRoutineExercisesEntity(RoutineEntity routine, Long routineExerciseId) throws NoSuchElementException, IllegalArgumentException {
+        RoutineExercisesEntity routineExercise = routineExercisesRepo.findById(routineExerciseId);
+        RoutineEntity routineRef;
+
+        if (routineExercise == null) {
+            throw new NoSuchElementException("Routine Exercise not found");
+        }
+
+        routineRef = routineExercise.getRoutineEntity();
+
+        if (routineRef != routine) {
+            throw new IllegalArgumentException("Exercise belongs to different routine");
+        }
+
+        return routineExercise;
+    }
 }
