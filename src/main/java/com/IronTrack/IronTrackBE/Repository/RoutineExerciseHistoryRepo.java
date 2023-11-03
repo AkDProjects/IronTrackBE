86c67d5fbcd3332e11969841a1679c1d0fe72b9d
canasmh <canasmh@yahoo.com>
2023-11-03 16:16:32 -0400

    Create RoutineExerciseHistoryRepo
    This repo will be used to perform CRUD operations inthe RoutineExerciseHistory entity

---
+++ src/main/java/com/IronTrack/IronTrackBE/Repository/RoutineExerciseHistoryRepo.java
@@ -0,0 +1,9 @@
+package com.IronTrack.IronTrackBE.Repository;
+
+import com.IronTrack.IronTrackBE.Repository.Entities.RoutineExerciseHistoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutineExerciseHistoryRepo extends JpaRepository<RoutineExerciseHistoryEntity, Long> {
+}
